adb216fdafeadeeba2dea7db76c2afd68b3aace4
Bruno <bruno.dill@protonmail.com>
2023-11-10 13:19:28 +0100

    added js for first question card

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,15 @@
+// Dark Mode
 const button = document.querySelector("#dark-mode");
 const body = document.querySelector('[data-type="body"]');
 
 button.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
+});
+
+// Show Answers
+const answerButton = document.querySelector("#show-answer-1");
+const answerText = document.querySelector("#answer-1");
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("hidden");
 });
