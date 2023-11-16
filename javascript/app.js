2854172336edca7e7f84f057a66efa182f4c9968
Bruno <bruno.dill@protonmail.com>
2023-11-16 10:38:15 +0100

    added function to show/hide anwers

--- javascript/app.js
+++ javascript/app.js
@@ -2,14 +2,32 @@
 const button = document.querySelector("#dark-mode");
 const body = document.querySelector('[data-type="body"]');
 
-button.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-});
+if (button) {
+  button.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+  });
+}
 
 // Show Answers
-const answerButton = document.querySelector("#show-answer-1");
-const answerText = document.querySelector("#answer-1");
+const answerButton1 = document.querySelector("#show-answer-1");
+const answerButton2 = document.querySelector("#show-answer-2");
 
-answerButton.addEventListener("click", () => {
-  answerText.classList.toggle("hidden");
-});
+if (answerButton1) {
+  answerButton1.addEventListener("click", () => {
+    hideNextSibling(answerButton1);
+  });
+}
+if (answerButton2) {
+  answerButton2.addEventListener("click", () => {
+    hideNextSibling(answerButton2);
+  });
+}
+
+function hideNextSibling(name) {
+  name.nextElementSibling.classList.toggle("hidden");
+  if (name.nextElementSibling.classList.contains("hidden")) {
+    name.textContent = "Show Answer";
+  } else {
+    name.textContent = "Hide Answer";
+  }
+}
