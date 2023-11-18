5d81428fd383cef2e56a2cfad83197ea644d1117
Bruno <bruno.dill@protonmail.com>
2023-11-18 17:37:16 +0100

    toggle aria hidden for answers

--- javascript/app.js
+++ javascript/app.js
@@ -19,10 +19,13 @@ function hideNextSibling(name) {
   const nextSibling = name.nextElementSibling;
   toggleClass(nextSibling, "hidden");
   toggleClass(nextSibling, "answer-appear");
+
   if (nextSibling.classList.contains("hidden")) {
     name.textContent = "Show Answer";
+    nextSibling.setAttribute("aria-hidden", "true");
   } else {
     name.textContent = "Hide Answer";
+    nextSibling.setAttribute("aria-hidden", "false");
   }
 }
 
