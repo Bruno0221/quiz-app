330d89c2c35b3ccf5ae84217d6032c4058db2ab5
Bruno <bruno.dill@protonmail.com>
2023-12-03 23:32:31 +0100

    cleanup

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -119,7 +119,6 @@ export function Footer() {
   const homeButton = document.querySelector('[data-js="home-page"]');
   const bookmarkedButton = document.querySelector('[data-js="bookmark-page"]');
   const profileButton = document.querySelector('[data-js="profile-page"]');
-  const navButtons = document.querySelectorAll('[data-js*="page"]');
 
   homeButton.addEventListener("click", renderMain);
   bookmarkedButton.addEventListener("click", renderBookmarked);
