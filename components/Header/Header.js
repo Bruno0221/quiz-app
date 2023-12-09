c431784012f00d98eaca0fbd42638f36bae1bb55
Bruno <bruno.dill@protonmail.com>
2023-12-09 12:43:00 +0100

    fixed header after element

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,11 @@
 import {
   createNewElement,
-  createNewElementWithClass,
   createNewElementWithClassAndContent,
 } from "../../utils/CreateNewElements.js";
 
 export function Header() {
   const root = document.querySelector("#root");
-  const header = createNewElementWithClass("header", root, "title");
+  const header = createNewElement("header", root);
   header.setAttribute("aria-labelledby", "title");
   const title = createNewElementWithClassAndContent(
     "h1",
